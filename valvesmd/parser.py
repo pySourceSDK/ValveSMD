67565d81627d1d4e87e4653caf0820e17d5c64ed
maxdup <mdupuis@hotmail.ca>
2021-08-27 09:39:23 -0400

    refactored parser

--- valvesmd/parser.py
+++ valvesmd/parser.py
@@ -16,7 +16,7 @@ from valvesmd.smd import *  # NOQA: E402
 
 
 def asTuple(data):
-    return tuple(data.asList()[0])
+    return [tuple(data.asList()[0])]
 
 
 def asList(data):
@@ -24,115 +24,68 @@ def asList(data):
 
 
 def asDict(data):
-    keys = [v for v in data.keys()]
-    if len(data) == len(keys):
-        return {keys[i]: value(data[i]) for i in range(len(keys))}
-    else:
-        return {k: value(data[k]) for k in keys}
+    return {k: value(v) for k, v in data.items()}
 
 
 def value(val):
     if isinstance(val, ParseResults):
-        return val.asList() or val.asDict() or val
+        while isinstance(val[0], ParseResults):
+            val = val[0]
+        if isinstance(val[0], tuple):
+            return val[0]
+        return val.asList() or val.asDict() or val[0]
     return val
 
 
 pp_comment = Literal('//') + SkipTo(lineEnd)
 pp_int = Word(nums+'-').setParseAction(lambda toks: int(toks[0]))
 pp_float = Word(nums+'-.').setParseAction(lambda toks: float(toks[0]))
+pp_float2 = Group(pp_float * 2).setParseAction(asTuple)
+pp_float3 = Group(pp_float * 3).setParseAction(asTuple)
 
+# Version
 pp_version = Suppress(ZeroOrMore(pp_comment)) + \
-    Suppress(CaselessLiteral('version')) + Word(nums)
-pp_version.setResultsName('version')
-
+    Suppress(CaselessLiteral('version')) + pp_int('version')
 
 # Triangles
-pp_material = Word(printables+' ')
-pp_material = pp_material.setResultsName('material')
+pp_link = Group(pp_int('boneid') + pp_float('weight'))
+pp_link.setParseAction(lambda l: SmdLink(asDict(l[0])))
+pp_links = countedArray(pp_link)('links')
 
-pp_pbone = pp_int.setResultsName('parent_boneid')
-
-pp_pos = Group(pp_float + pp_float + pp_float)
-pp_pos = pp_pos.setResultsName(
-    'position').setParseAction(asTuple)
-
-pp_norm = Group(pp_float + pp_float + pp_float)
-pp_norm = pp_norm.setResultsName('normal').setParseAction(asTuple)
-
-pp_uv = Group(pp_float + pp_float)
-pp_uv = pp_uv.setResultsName('uv').setParseAction(asTuple)
-
-pp_boneid = pp_int.setResultsName('boneid')
-pp_weight = pp_float.setResultsName('weight')
-
-pp_link = Group(pp_boneid + pp_weight)
-pp_link = pp_link.setParseAction(lambda l: SmdLink(asDict(l[0])))
-
-pp_links = countedArray(pp_link)
-pp_links = pp_links.setResultsName('links')
-
-pp_vert_base = pp_pbone + pp_pos + pp_norm + pp_uv
+pp_vert_base = pp_int('parent_boneid') + pp_float3('position') + \
+    pp_float3('normal') + pp_float2('uv')
 pp_vert_base.setParseAction(lambda v: SmdVert(asDict(v)))
 
-pp_vert_extended = pp_pbone + pp_pos + pp_norm + pp_uv + pp_links
+pp_vert_extended = pp_int('parent_boneid') + pp_float3('position') + \
+    pp_float3('normal') + pp_float2('uv') + pp_links
 pp_vert_extended.setParseAction(lambda v: SmdVert(asDict(v)))
 
-pp_verts_base = Group(
-    pp_vert_base + pp_vert_base + pp_vert_base)
-pp_verts_extended = Group(
-    pp_vert_extended + pp_vert_extended + pp_vert_extended)
-pp_verts_base.setParseAction(asTuple)
-pp_verts_extended.setParseAction(asTuple)
+pp_verts_base = Group(pp_vert_base * 3).setParseAction(asTuple)
+pp_verts_extended = Group(pp_vert_extended * 3).setParseAction(asTuple)
+pp_verts = pp_verts_extended('verts') ^ pp_verts_base('verts')
 
-pp_verts = pp_verts_extended ^ pp_verts_base
-pp_verts = pp_verts.setResultsName('verts')
-
-pp_triangle = Group(pp_material + pp_verts)
+pp_triangle = Group(Word(printables+' ')('material') + pp_verts('verts'))
 pp_triangle.addParseAction(lambda t: SmdTriangle(asDict(t[0])))
 
-pp_triangles = Group(Suppress('triangles') +
-                     ZeroOrMore(pp_triangle) + Suppress('end'))
-pp_triangles = pp_triangles.setResultsName('triangles')
-pp_triangles = pp_triangles.setParseAction(asList)
+pp_triangles = Group(Suppress('triangles') + ZeroOrMore(pp_triangle) +
+                     Suppress('end'))('triangles').setParseAction(asList)
 
 # Nodes
-pp_nid = pp_int.setResultsName('id')
-pp_nname = QuotedString('"').setResultsName('name')
-pp_nparent_id = pp_int.setResultsName('parent_id')
-
-pp_node = pp_nid + pp_nname + pp_nparent_id
-pp_node = pp_node.setParseAction(lambda n: SmdNode(asDict(n)))
-
-pp_nodes = Group(Suppress('nodes') + ZeroOrMore(pp_node) + Suppress('end'))
-pp_nodes = pp_nodes.setResultsName('nodes')
-pp_nodes = pp_nodes.setParseAction(asList)
+pp_node = pp_int('id') + QuotedString('"')('name') + pp_int('parent_id')
+pp_node.setParseAction(lambda n: SmdNode(asDict(n)))
+pp_nodes = Group(Suppress('nodes') + ZeroOrMore(pp_node) +
+                 Suppress('end'))('nodes').setParseAction(asList)
 
 # Skeleton
-pp_framedef = pp_int.setResultsName('frame')
+pp_bonepose = pp_int('boneid') + pp_float3('position') + pp_float3('rotation')
+pp_bonepose.setParseAction(lambda p: SmdBonePose(asDict(p)))
+pp_boneposes = Group(ZeroOrMore(pp_bonepose))('poses').setParseAction(asList)
 
-pp_sbid = pp_int.setResultsName('boneid')
+pp_keyframe = Suppress('time') + pp_int('frame') + pp_boneposes
+pp_keyframe.setParseAction(lambda k: SmdKeyframe(asDict(k)))
 
-pp_spos = Group(pp_float + pp_float + pp_float)
-pp_spos = pp_norm.setResultsName('position').setParseAction(asTuple)
-
-pp_srot = Group(pp_float + pp_float + pp_float)
-pp_srot = pp_norm.setResultsName('rotation').setParseAction(asTuple)
-
-pp_bonepose = pp_sbid + pp_spos + pp_srot
-pp_bonepose = pp_bonepose.setParseAction(lambda p: SmdBonePose(asDict(p)))
-
-pp_boneposes = Group(ZeroOrMore(pp_bonepose))
-pp_boneposes = pp_boneposes.setResultsName('poses')
-pp_boneposes = pp_boneposes.setParseAction(asList)
-
-pp_keyframe = Suppress('time') + pp_framedef + pp_boneposes
-pp_keyframe = pp_keyframe.setParseAction(lambda k: SmdKeyframe(asDict(k)))
-
-pp_skeleton = Group(Suppress('skeleton') +
-                    ZeroOrMore(pp_keyframe) + Suppress('end'))
-pp_skeleton = pp_skeleton.setResultsName('skeleton')
-pp_skeleton = pp_skeleton.setParseAction(asList)
-
+pp_skeleton = Group(Suppress('skeleton') + ZeroOrMore(pp_keyframe) +
+                    Suppress('end'))('skeleton').setParseAction(asList)
 
 pp_smd = Group(pp_version + pp_nodes + pp_skeleton + pp_triangles)
 pp_smd.setParseAction(lambda s: SmdRoot(asDict(s[0])))
@@ -144,7 +97,6 @@ class SmdSyntaxError(Exception):
 
 def SmdParse(filename):
     results = []
-
     try:
         f = open(filename, "r", encoding="iso-8859-1")
         try:
